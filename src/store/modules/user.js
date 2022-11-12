4093f6e9af11996a5dcf7bf2a88e1f52abe089ed
lekeRonin <yujianlove020216@163.com>
2022-11-12 14:39:47 +0800

    完成了登录页面和主页的一些布局

---
+++ src/store/modules/user.js
@@ -0,0 +1,127 @@
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
+const state = {
+  token: getToken() // 设置token为共享状态
+}
+// 修改状态
+const mutations = {
+  setToken(state, token) {
+    state.token = token // 设置token  只是修改state的数据  123 =》 1234
+    // vuex变化 => 缓存数据
+    setToken(token) // vuex和 缓存数据的同步
+  },
+  removeToken(state) {
+    state.token = null // 删除vuex的token
+    removeToken() // 先清除 vuex  再清除缓存 vuex和 缓存数据的同步
+  }
+}
+// 执行异步
+const actions = {
+  // 定义login action  也需要参数 调用action时 传递过来的参数
+  async login(context, data) {
+    const result = await login(data) // 实际上就是一个promise  result就是执行的结果
+    // axios默认给数据加了一层data
+    // if (result.data.success) {
+    //   // 表示登录接口调用成功 也就是意味着你的用户名和密码是正确的
+    //   // 现在有用户token
+    //   // actions 修改state 必须通过mutations
+    //   context.commit('setToken', result.data.data)
+    // }
+    context.commit('setToken', result)
+  }
+}
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+// import { login, logout, getInfo } from '@/api/user'
+// import { getToken, setToken, removeToken } from '@/utils/auth'
+// import { resetRouter } from '@/router'
+
+// const getDefaultState = () => {
+//   return {
+//     token: getToken(),
+//     name: '',
+//     avatar: ''
+//   }
+// }
+
+// const state = getDefaultState()
+
+// const mutations = {
+//   RESET_STATE: (state) => {
+//     Object.assign(state, getDefaultState())
+//   },
+//   SET_TOKEN: (state, token) => {
+//     state.token = token
+//   },
+//   SET_NAME: (state, name) => {
+//     state.name = name
+//   },
+//   SET_AVATAR: (state, avatar) => {
+//     state.avatar = avatar
+//   }
+// }
+
+// const actions = {
+//   // user login
+//   login({ commit }, userInfo) {
+//     const { username, password } = userInfo
+//     return new Promise((resolve, reject) => {
+//       login({ username: username.trim(), password: password }).then(response => {
+//         const { data } = response
+//         commit('SET_TOKEN', data.token)
+//         setToken(data.token)
+//         resolve()
+//       }).catch(error => {
+//         reject(error)
+//       })
+//     })
+//   },
+
+//   // get user info
+//   getInfo({ commit, state }) {
+//     return new Promise((resolve, reject) => {
+//       getInfo(state.token).then(response => {
+//         const { data } = response
+
+//         if (!data) {
+//           return reject('Verification failed, please Login again.')
+//         }
+
+//         const { name, avatar } = data
+
+//         commit('SET_NAME', name)
+//         commit('SET_AVATAR', avatar)
+//         resolve(data)
+//       }).catch(error => {
+//         reject(error)
+//       })
+//     })
+//   },
+
+//   // user logout
+//   logout({ commit, state }) {
+//     return new Promise((resolve, reject) => {
+//       logout(state.token).then(() => {
+//         removeToken() // must remove  token  first
+//         resetRouter()
+//         commit('RESET_STATE')
+//         resolve()
+//       }).catch(error => {
+//         reject(error)
+//       })
+//     })
+//   },
+
+//   // remove token
+//   resetToken({ commit }) {
+//     return new Promise(resolve => {
+//       removeToken() // must remove  token  first
+//       commit('RESET_STATE')
+//       resolve()
+//     })
+//   }
+// }
